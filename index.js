119c7534828f582146125d3951e72cc374244fd4
Hardik0078 <hardik.p@enjayworld.com>
2026-06-06 17:42:33 +0530

    add commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World! after deploy\n');
+  res.end('App deployed successfully !\n');
 });
 
 server.listen(3002, () => {
