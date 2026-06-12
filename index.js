aab02644aa783036f162ad41c975a0a4a5b57886
Hardik0078 <hardik.p@enjayworld.com>
2026-06-12 14:39:12 +0530

    code add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('App deployed successfully !\n');
+  res.end('App deployed successfully on k8s app !\n');
 });
 
 server.listen(3002, () => {
